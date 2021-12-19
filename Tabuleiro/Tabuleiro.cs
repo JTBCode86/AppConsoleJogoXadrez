1555ed92d5e9a8a6244b79e0399a1c85a3c90421
Jean Barbosa <jeanbarbosa.it@gmail.com>
2021-12-19 12:22:44 -0300

    Começando a implementar o metodo para imprimir o tabuleiro.

--- Tabuleiro/Tabuleiro.cs
+++ Tabuleiro/Tabuleiro.cs
@@ -14,5 +14,10 @@
             this.colunas = colunas;
             pecas = new Peca[linhas, colunas];
         }
+
+        public Peca Peca(int linha, int coluna) 
+        {
+            return pecas[linha, coluna];
+        }
     }
 }
