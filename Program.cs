472530046e23cede24fa486e575a5145fde69eea
Jean Barbosa <jeanbarbosa.it@gmail.com>
2021-12-19 15:46:41 -0300

    Implementação da classe PosicaoXadrez.

--- Program.cs
+++ Program.cs
@@ -8,19 +8,10 @@ namespace AppConsoleJogoXadrez
     {
         static void Main(string[] args)
         {
-            try
-            {
-                Tabuleiro tab = new Tabuleiro(8, 8);
-                tab.colocarPeca(new Torre(tab, Cor.preta), new Posicao(0, 0));
-                tab.colocarPeca(new Torre(tab, Cor.preta), new Posicao(1, 9));
-                tab.colocarPeca(new Rei(tab, Cor.preta), new Posicao(0,2));
-                Tela.imprimirTabuleiro(tab);
-            }
+            PosicaoXadrez posicaoXadrez = new PosicaoXadrez('c', 7);
+            Console.WriteLine(posicaoXadrez);
+            Console.WriteLine(posicaoXadrez.ToPosisao());
 
-            catch (TabuleiroException e)
-            {
-                Console.WriteLine(e.Message);
-            }
             Console.ReadLine();
         }
     }
