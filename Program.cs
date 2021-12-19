71e1130b5aa1b25f9da0278fa89eb56181e342fb
Jean Barbosa <jeanbarbosa.it@gmail.com>
2021-12-19 11:51:34 -0300

    Criação da classe posição

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Tabuleiro;
 
 namespace AppConsoleJogoXadrez
 {
@@ -6,7 +7,10 @@ namespace AppConsoleJogoXadrez
     {
         static void Main(string[] args)
         {
-            
+            Posicao p;
+            p = new Posicao(3, 4);
+            Console.WriteLine("Posição: " + p);
+            Console.ReadLine();
         }
     }
 }
